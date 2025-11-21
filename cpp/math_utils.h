ca71fe5eea2425edfa565fb7245bd3f7e9cbfe70
AnalogArnold <michalvx3@gmail.com>
2025-11-21 10:32:46 +0000

    First commit

---
+++ cpp/math_utils.h
@@ -0,0 +1,17 @@
+#pragma once // Header guard instead of ifndef
+
+#include <cmath>
+#include <random>
+//#include <corecrt_math_defines.h> // Otherwise M_PI doesn't work in VS. cmath alone works in CLion, though.
+#include "eigen_types.h"
+
+inline double degreesToRadians(double angleDeg) {
+    return angleDeg * M_PI / 180;
+}
+
+
+static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+static std::mt19937 generator;
+inline double random_double() {
+    return distribution(generator);
+}
---
+++ cpp/test1.cpp
@@ -0,0 +1,101 @@
+#include <cmath>
+#include "./Eigen/Dense"
+#include <cstdint>
+
+#include <fstream>
+#include <iostream>
+#include <array>
+#include <vector>
+
+
+
+#include "eigen_types.h"
+#include "render.h"
+
+/*
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+void render_scene(const int image_height,
+    const int image_width,
+    const int number_of_samples,
+    const std::vector<py::array_t<int>> &scene_connectivity,
+    const std::vector<py::array_t<double>> &scene_coords,
+    const std::vector<pybind11::array_t<double>>& scene_face_colors,
+    const std::vector<Eigen::Ref<const EiVector3d>> camera_centers,
+    const std::vector<Eigen::Ref<const EiVector3d>> pixel_00_centers,
+    const std::vector<Eigen::Ref<const Eigen::Matrix<double, 2, 3, Eigen::StorageOptions::RowMajor>>> matrix_pixel_spacings) {
+
+    size_t num_cameras = camera_centers.size();
+
+    // Iterate over all cameras and render an image for each
+    for (size_t camera_idx = 0; camera_idx < num_cameras; ++camera_idx) {
+        Eigen::Ref<const EiVector3d> camera_center = camera_centers[camera_idx];
+        Eigen::Ref<const EiVector3d> pixel_00_center = pixel_00_centers[camera_idx];
+        Eigen::Ref<const Eigen::Matrix<double, 2, 3, Eigen::StorageOptions::RowMajor>> matrix_pixel_spacing = matrix_pixel_spacings[camera_idx];
+
+        // Get bytes from the render function and pass back to Python to write it to a file from there
+        //return render_ppm_image(test_camera, connectivity, node_coords);
+        render_ppm_image(camera_center, pixel_00_center, matrix_pixel_spacing, scene_connectivity, scene_coords, scene_face_colors, image_height, image_width, number_of_samples);
+    }
+}
+
+PYBIND11_MODULE(superfastcode, a) {
+    a.def("cpp_render_scene", &render_scene);
+}
+*/
+
+#include <nanobind/nanobind.h>
+#include <nanobind/eigen/dense.h>
+#include <nanobind/ndarray.h>
+#include <nanobind/stl/vector.h>
+#include <nanobind/nanobind.h>
+
+namespace nb = nanobind;
+/*
+void render_scene(const int number){
+
+	std::cout << "Number from Python: " << number << std::endl;
+}
+
+NB_MODULE(superfastcode, a) {
+    a.def("cpp_render_scene", &render_scene);
+}
+    
+*/
+
+
+void render_scene(const int image_height,
+    const int image_width,
+    const int number_of_samples,
+    const std::vector<nb::ndarray<const int, nb::c_contig>>& scene_connectivity,
+    const std::vector<nb::ndarray<const double, nb::c_contig>>& scene_coords,
+    const std::vector<nb::ndarray<const double, nb::c_contig>>& scene_face_colors,
+    const std::vector<nb::DRef<EiVector3d>> camera_centers,
+    const std::vector<nb::DRef<EiVector3d>> pixel_00_centers,
+    const std::vector<nb::DRef<Eigen::Matrix<double, 2, 3, Eigen::StorageOptions::RowMajor>>> matrix_pixel_spacings) {
+
+    size_t num_cameras = camera_centers.size();
+    //std::cout << "Made it to the cpp" << std::endl;
+
+    // Iterate over all cameras and render an image for each
+    for (size_t camera_idx = 0; camera_idx < num_cameras; ++camera_idx) {
+        EiVector3d camera_center = camera_centers[camera_idx];
+        EiVector3d pixel_00_center = pixel_00_centers[camera_idx];
+        Eigen::Matrix<double, 2, 3, Eigen::StorageOptions::RowMajor> matrix_pixel_spacing = matrix_pixel_spacings[camera_idx];
+
+        //std::cout << "we're in the loop" << std::endl;
+
+        // Get bytes from the render function and pass back to Python to write it to a file from there
+        //return render_ppm_image(test_camera, connectivity, node_coords);
+        render_ppm_image(camera_center, pixel_00_center, matrix_pixel_spacing, scene_connectivity, scene_coords, scene_face_colors, image_height, image_width, number_of_samples);
+    }
+}
+
+NB_MODULE(superfastcode, a) {
+    a.def("cpp_render_scene", &render_scene);
+}
